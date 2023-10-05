dfe2fee9b495965fd7c088d4c7b62f7cbc572a13
Leonardo munhos sanfelice <126088427+leosanfelice@users.noreply.github.com>
2023-10-04 21:53:06 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
-document.write("Olá mundo");
-document.write("Vamos aprender javascript!");
+// document.write("Ola mundo");
+// console.log("outro ola mundo");
+// alert("Ola mundo com alert");
 
+// let idade = 14;
+// let altura = 1.79;
+// let peso = 50.5;
+// let mensagem = "aprender js eh dificil";
+
+// document.write("<p> idade " + idade + "</p>");
+// document.write(`altura: ${altura}`);
+// document.write("<p> peso " + peso + "</p>");
+// document.write(`mensagem: ${mensagem}`);
+
+// let n1 = 10;
+// let n2 = 2;
+
+// document.write(`n1 ${n1} <br>`);
+// document.write(`n2 ${n2} <br>`);
+
+// console.log(n1 + n2);
+// console.log(n1 - n2);
+// console.log(n1 * n2);
+// console.log(n1 / n2);
+
+// console.log(n1 == n2);
+// console.log(n1 === n2);
+// console.log(n1 != n2);
+// console.log(n1 !== n2);
