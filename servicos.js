06edce48e5c5bc4b8e4df0cdb93075f6e30abf09
Leonardo munhos sanfelice <126088427+leosanfelice@users.noreply.github.com>
2024-03-04 16:15:45 -0300

    Add files via upload

---
+++ servicos.js
@@ -0,0 +1,18 @@
+function enviar() {
+
+    let nome = document.querySelector("#nome")
+    let senha = document.querySelector("#senha")
+    let telefone = document.querySelector("#telefone")
+    let assunto = document.querySelector("#assunto")
+
+    console.log(nome.value)
+    console.log(senha.value)
+    console.log(telefone.value)
+    console.log(assunto.value)
+
+    if(nome.value == 0 || senha.value == 0 || telefone.value == 0 || assunto.value == 0) {
+        alert("Algum campo não preenchido!")
+    } else {
+        alert("Enviado")
+    }
+}
